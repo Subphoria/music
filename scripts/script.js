5bbcef6c60d2795dcea7b42d8280b827a791259f
Subphoria <loanne.subphoria@gmail.com>
2026-01-31 23:45:00 +0100

    2025-01-31 suppression avant update

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-function loadComponent(id, file) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        })
-        .catch(error => console.error('Erreur lors du chargement :', error));
-}
-
